0ea85b631d3add681836e23b05df34e87d863cf1
Domtuber <80555127+Domtuber@users.noreply.github.com>
2021-04-24 02:20:21 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-#   v0.0.4
+#   v0.0.5
 
 import discord
 from discord.ext import commands
@@ -9,7 +9,7 @@ import random
 token = "ODE5OTQ3NDc4ODQ0NTcxNzQ5.YEuBmA.IPV09eEIHnlKUszD3WSHPbCzhSA"
 os.chdir(r"C:\Users\danie\PycharmProjects\yt_vid_downloading\remind_bot")
 intents = discord.Intents.all()
-version = "v0.0.4"
+version = "v0.0.5"
 link = "https://discord.com/api/oauth2/authorize?client_id=819947478844571749&permissions=8&scope=bot"
 prefix_ = "r,"
 bot = commands.Bot(command_prefix=prefix_, intents=intents)
@@ -170,29 +170,133 @@ async def battle(ctx):
     def check(reaction, user):
         return user == ctx.author and (reaction.message == x and str(reaction) == '🗡️' or str(reaction) == '🛡️')
 
-    try:
-        reaction, user = await bot.wait_for("reaction_add", timeout=30.0, check=check)
-    except asyncio.TimeoutError:
-        await ctx.send("The battle has timed out.")
-    else:
-        if str(reaction) == '🗡️':
-            miss = random.randint(1, 6)
-            if miss == 5:
-                await ctx.send(f"{ctx.author.name} missed!")
-            damage = random.randrange(5, atk, 5)
+    bot_defended = False
+    turn = 'user'
+    while bot_hp > 0 or hp_amt > 0:
+        defended = False
+        bot_armor = 0
+        # user's turn
+        if turn == 'user':
+            if bot_defended is True:
+                bot_armor = 5
+            try:
+                reaction, user = await bot.wait_for("reaction_add", timeout=120.0, check=check)
+            except asyncio.TimeoutError:
+                await ctx.send("The battle has timed out.")
+                return
+            else:
+                if str(reaction) == '🗡️':
+                    miss = random.randint(1, 6)
+                    if miss == 5:
+                        mis = await ctx.send(f"{ctx.author.name} missed!")
+                        await x.clear_reactions()
+                        turn = 'bot'
+                        await asyncio.sleep(1)
+                        await mis.delete()
+                        continue
+                    damage = random.randrange(5, atk, 5) - int(bot_armor)
+                    if damage < 0:
+                        damage = 0
+                    bot_hp -= damage
 
-            bot_hp -= damage
+                    if bot_hp < 0:
+                        bot_hp = 0
 
-            e = discord.Embed(title="Practice Battle", color=discord.Color.orange())
-            e.add_field(name=f"{ctx.author.name} *attacked!*", value=str(hp_amt) + " HP")
-            e.add_field(name="RPG(bot)", value='- **' + str(damage) + '** ' + str(bot_hp) + " HP")
-            e.set_footer(
-                text="You are now in a practice battle with the bot! React with the sword to attack or the shield to defend.")
+                    e = discord.Embed(title="Practice Battle", color=discord.Color.orange())
+                    e.add_field(name=f"{ctx.author.name} *attacked!*", value=str(hp_amt) + " HP")
+                    e.add_field(name="RPG(bot)", value='- **' + str(damage) + '** ' + str(bot_hp) + " HP")
+                    e.set_footer(
+                        text="You are now in a practice battle with the bot! React with the sword to attack or the shield to defend.")
+                    await x.edit(embed=e)
 
-            await x.edit(embed=e)
+                    bot_defended = False
 
-        elif str(reaction) == '🛡️':
-            await ctx.send('pp')
+                    await x.clear_reactions()
+
+                    turn = 'bot'
+
+                    if bot_hp <= 0:
+                        e = discord.Embed(title="Practice Battle", color=discord.Color.orange())
+                        e.add_field(name=f"{ctx.author.name} *wins!*", value=str(hp_amt) + " HP")
+                        e.add_field(name="RPG(bot)", value='- **' + str(damage) + '** ' + str(bot_hp) + " HP")
+                        await x.edit(embed=e)
+                        break
+
+                elif str(reaction) == '🛡️':
+                    e = discord.Embed(title="Practice Battle", color=discord.Color.orange())
+                    e.add_field(name=f"{ctx.author.name} *defended!*", value=str(hp_amt) + " HP")
+                    e.add_field(name="RPG(bot)", value=str(bot_hp) + " HP")
+                    e.set_footer(
+                        text="You are now in a practice battle with the bot! React with the sword to attack or the shield to defend.")
+                    await x.edit(embed=e)
+
+                    defended = True
+
+                    bot_defended = False
+
+                    await x.clear_reactions()
+
+                    turn = 'bot'
+
+        # bot's turn
+
+        if turn == 'bot':
+            if defended is True:
+                armor_rating += armor_rating*1.5
+
+            action = random.choice(['attack', 'defend'])
+            if action == 'attack':
+                miss = random.randint(1, 6)
+                if miss == 5:
+                    mis = await ctx.send("RPG(bot) missed!")
+                    await x.add_reaction(emoji1)
+                    await x.add_reaction(emoji2)
+                    await asyncio.sleep(1)
+                    await mis.delete()
+                    turn = 'user'
+                    continue
+
+                damage = random.randrange(5, 30, 5) - int(armor_rating)
+                if damage < 0:
+                    damage = 0
+                hp_amt -= damage
+
+                if hp_amt < 0:
+                    hp_amt = 0
+
+                e = discord.Embed(title="Practice Battle", color=discord.Color.orange())
+                e.add_field(name=f"{ctx.author.name}", value='- **' + str(damage) + '** ' + str(hp_amt) + " HP")
+                e.add_field(name="RPG(bot) *attacked!*", value=str(bot_hp) + " HP")
+                e.set_footer(
+                    text="You are now in a practice battle with the bot! React with the sword to attack or the shield to defend.")
+                await x.edit(embed=e)
+
+                await x.add_reaction(emoji1)
+                await x.add_reaction(emoji2)
+
+                if hp_amt <= 0:
+                    e = discord.Embed(title="Practice Battle", color=discord.Color.orange())
+                    e.add_field(name=f"{ctx.author.name}", value='- **' + str(damage) + '** ' + str(hp_amt) + " HP")
+                    e.add_field(name="RPG(bot) *wins!*", value=str(bot_hp) + " HP")
+                    await x.edit(embed=e)
+                    break
+
+                turn = 'user'
+
+            elif action == 'defend':
+                e = discord.Embed(title="Practice Battle", color=discord.Color.orange())
+                e.add_field(name=f"{ctx.author.name}", value=str(hp_amt) + " HP")
+                e.add_field(name="RPG(bot) *defended!*", value=str(bot_hp) + " HP")
+                e.set_footer(
+                    text="You are now in a practice battle with the bot! React with the sword to attack or the shield to defend.")
+                await x.edit(embed=e)
+
+                bot_defended = True
+
+                await x.add_reaction(emoji1)
+                await x.add_reaction(emoji2)
+
+                turn = 'user'
 
 
 bot.run(token)
